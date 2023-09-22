f648081de27438b89a5438a6bc914106309e8a93
julidoominguez <julieta.dominguez28@gmail.com>
2023-09-22 21:54:25 +0000

    codigo corregido

--- src/workflow-inicial/621_DR_corregir_drifting.r
+++ src/workflow-inicial/621_DR_corregir_drifting.r
@@ -238,15 +238,15 @@ drift_rank_cero_fijo <- function(campos_drift) {
 #------------------------------------------------------------------------------
 
 drift_estandarizar <- function(campos_drift) {
-  for (campo in campos_drift)
-  {
-    cat(campo, " ")
-    dataset[, paste0(campo, "_normal") := 
-      (get(campo) -mean(campo, na.rm=TRUE)) / sd(get(campo), na.rm=TRUE),
-      by = foto_mes]
+for (campo in campos_drift)
+{
+cat(campo, " ")
+dataset[, paste0(campo, "_normal") :=
+(get(campo) - mean(get(campo), na.rm=TRUE)) / sd(get(campo), na.rm=TRUE),
+by = foto_mes]
 
-    dataset[, (campo) := NULL]
-  }
+dataset[, (campo) := NULL]
+}
 }
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
